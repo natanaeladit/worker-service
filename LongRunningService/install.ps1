feee6c19a0aebb3e8e690861d8586cdb8a6eeaa3
natanaeladit <nata.adit@gmail.com>
2020-03-08 18:49:18 +0800

    serverless

---
+++ LongRunningService/install.ps1
@@ -0,0 +1,13 @@
+﻿$serviceName = "LongRunningService"
+$displayName = "Long Running Service"
+$description = "Long Running Service"
+$currentDir = get-location
+$binaryPath = "$($currentDir)\LongRunningService.exe"
+$credential = "$($env:computername)\$($env:username)"
+$acl = Get-Acl $currentDir
+$aclRuleArgs = $credential, "Read,Write,ReadAndExecute", "ContainerInherit,ObjectInherit", "None", "Allow"
+$accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($aclRuleArgs)
+$acl.SetAccessRule($accessRule)
+$acl | Set-Acl $currentDir
+
+New-Service -Name $serviceName -BinaryPathName $binaryPath -Credential $credential -Description $description -DisplayName $displayName -StartupType Automatic
